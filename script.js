cd5cefbe2aa2023a1f25a6b72740389ddd08d1a8
Marcos <marcos.jinno@gmail.com>
2025-02-26 13:00:13 -0300

    Agrega test cypress

--- script.js
+++ script.js
@@ -1,36 +1,49 @@
-$("body").addClass("fondo");
-
-const ultimasCotizacionesURL = "https://api.frankfurter.dev/v1/latest";
+const ultimasCotizacionesURL = 'https://api.frankfurter.dev/v1/latest';
 obtenerTiposDeCambio();
 
-const $botonBuscarCambios = $("#buscar-btn");
+const $botonBuscarCambios = $('#buscar-btn');
+const $botonBorrarLista = $('#borrar-btn');
 
-$botonBuscarCambios.on("click", () => {
+$botonBuscarCambios.on('click', () => {
+  $('#mensaje').text('Cargando tipos de cambio... ');
   obtenerFechaYBase();
+});
+
+$botonBorrarLista.on('click', () => {
+  $('#mensaje').text('');
+  $('#lista-cambios').html('');
+  $('#tipos-cambio').val('optionSelected');
+  $('#fecha-input').val('');
+
+  URL = '';
+
+  $('#buscar-btn').attr('disabled', false);
 });
 
 function obtenerTiposDeCambio() {
   fetch(ultimasCotizacionesURL)
-    .then((respuesta) => respuesta.json())
+    .then((respuesta) => {
+      if (!respuesta.ok) {
+        console.error(`Error: ${respuesta.status}`);
+      }
+      return respuesta.json();
+    })
     .then((data) => {
       const keys = Object.keys(data.rates);
       keys.forEach((moneda) => {
-        const opcion = $("<option>");
+        const opcion = $('<option>');
         opcion.text(moneda);
 
-        $("#tipos-cambio").append(opcion);
+        $('#tipos-cambio').append(opcion);
       });
     });
 }
 
 function obtenerFechaYBase() {
-  $("#mensaje").text("Cargando tipos de cambio... ");
+  const fecha = $('#fecha-input').val();
+  const base = $('#tipos-cambio').val();
 
-  const fecha = $("#fecha-input").val();
-  const base = $("#tipos-cambio").val();
-
-
-  const URL = `https://api.frankfurter.dev/v1/${fecha}?base=${base}`;
+  let URL = `https://api.frankfurter.dev/v1/${fecha}?base=${base}`;
 
   fetch(URL)
     .then((respuesta) => {
@@ -39,25 +52,26 @@ function obtenerFechaYBase() {
       }
       return respuesta.json();
     })
-    
+
     .then((data) => {
       for (const key in data) {
-        if (typeof data[key] === "object") {
+        if (typeof data[key] === 'object') {
           const denominacionDeMonedas = Object.keys(data[key]);
           const cotizacionesDeMonedas = Object.values(data[key]);
 
           denominacionDeMonedas.forEach((moneda, index) => {
-            $("#lista-cambios").append(
-              $("<li>").text(`${moneda} : ${cotizacionesDeMonedas[index]}`)
+            $('#lista-cambios').append(
+              $('<li>').text(`${moneda} : ${cotizacionesDeMonedas[index]}`),
             );
           });
         }
       }
+      $('#mensaje').text('');
     })
 
     .catch((error) => {
-      console.log(`Ocurrió un error: ${error}`);
+      console.error(`Ocurrió un error: ${error}`);
     });
 
-  $("#mensaje").text("");
+  $('#buscar-btn').attr('disabled', true);
 }
